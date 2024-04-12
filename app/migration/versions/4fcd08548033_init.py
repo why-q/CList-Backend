5f0f6a72e117b992187b3d0bd9f10dcc7bb29b6d
louis <louis@borges>
2024-04-12 15:34:06 +0800

    feat: Add alembic migrate.

---
+++ app/migration/versions/4fcd08548033_init.py
@@ -0,0 +1,25 @@
+"""init
+
+Revision ID: 4fcd08548033
+Revises: 
+Create Date: 2024-04-11 02:19:07.705124
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = '4fcd08548033'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
